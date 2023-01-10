41f0cca43ae70620747e0ad7bb934702c0e36af6
Frank Wall <fw@moov.de>
2023-01-10 16:27:19 +0100

    fix default user/group on Debian/Ubuntu

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [v1.2.0] - 2023-01-10
+
+### Fixed
+* Fix default user/group on Debian/Ubuntu
+
 ## [v1.1.1] - 2022-09-20
 
 ### Fixed
@@ -19,9 +24,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Fixed
 * Fix unit test
 
-## v1.0.0 - 2022-11-02
+## v1.0.0 - 2020-11-02
 Initial release
 
-[Unreleased]: https://github.com/markt-de/puppet-lam/compare/v1.1.1...HEAD
+[Unreleased]: https://github.com/markt-de/puppet-lam/compare/v1.2.0...HEAD
+[v1.2.0]: https://github.com/markt-de/puppet-lam/compare/v1.1.1...v1.2.0
 [v1.1.1]: https://github.com/markt-de/puppet-lam/compare/v1.1.0...v1.1.1
 [v1.1.0]: https://github.com/markt-de/puppet-lam/compare/v1.0.0...v1.1.0
--- spec/acceptance/lam_spec.rb
+++ spec/acceptance/lam_spec.rb
@@ -8,15 +8,11 @@ describe 'lam' do
         package { 'httpd':
             ensure => 'installed',
         }
-        $user_name = 'apache'
-        $group_name = 'apache'
       }
       else {
         package { 'apache2':
-            ensure => 'installed',
+          ensure => 'installed',
         }
-        $user_name = 'www-data'
-        $group_name = 'www-data'
       }
 
       package { 'bzip2':
@@ -24,16 +20,7 @@ describe 'lam' do
       }
 
       class { 'lam':
-        datadir        => '/opt/lam-data',
-        edition        => 'oss',
-        group          => $group_name,
-        installroot    => '/opt',
-        manage_symlink => true,
-        mirror         => 'http://prdownloads.sourceforge.net/lam/%s?download',
-        path           => '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin',
-        symlink_name   => 'lam',
-        user           => $user_name,
-        version        => '8.2',
+        version => '8.2',
       }
       EOS
 
